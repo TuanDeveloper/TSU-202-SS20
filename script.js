02dc6fdc25f3760636e36294f7d1700643aa91af
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-04-06 20:44:17 +0700

    register

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// let email = document.getElementById("email");
-// let password = document.getElementById("password");
-// let form = document.getElementById("form-login");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.getElementById("form-register");
 
-// form.addEventListener("submit", function (event) {
-//     // Ngan chan hanh vi mac dinh cua form
-//     event.preventDefault();
-//     // let emailValue = email.value.trim();
-//     // let passwordValue = password.value.trim();
+form.addEventListener("submit", function (event) {
+    // Ngan chan hanh vi mac dinh cua form
+    event.preventDefault();
+    // let emailValue = email.value.trim();
+    // let passwordValue = password.value.trim();
 
-//     let emailValue = form.email.value.trim();
-//     let passwordValue = form.password.value.trim();
-//     const newUser = {
-//         email: emailValue,
-//         password: passwordValue
-//     }
-//     console.log(newUser);
-// })
+    let emailValue = form.email.value.trim();
+    let passwordValue = form.password.value.trim();
+    const newUser = {
+        email: emailValue,
+        password: passwordValue
+    }
+    console.log(newUser);
+})
 
-let userName = "Quoc Tuan 123";
-let age = 30;
+// let userName = "Quoc Tuan 123";
+// let age = 30;
 
-let user = {
-    email: "it.coder.qtuan@gmail.com",
-    password: 123456,
-}
+// let user = {
+//     email: "it.coder.qtuan@gmail.com",
+//     password: 123456,
+// }
 
-localStorage.setItem("userName", userName);
-// localStorage.setItem("age", age);
-// Chuyển đổi object thành dữ liệu dạng JSON
-// Sau khi lưu vào Storage thì browser chuyển thành string và lưu
-localStorage.setItem("user", JSON.stringify(user));
+// localStorage.setItem("userName", userName);
+// // localStorage.setItem("age", age);
+// // Chuyển đổi object thành dữ liệu dạng JSON
+// // Sau khi lưu vào Storage thì browser chuyển thành string và lưu
+// localStorage.setItem("age", JSON.stringify(age));
 
-// localStorage.removeItem("userName");
-// Khi lấy về thì đang ở dữ liệu string, phải parse để chuyển thành Object
-let value = JSON.parse(localStorage.getItem("user"));
+// // localStorage.removeItem("userName");
+// // Khi lấy về thì đang ở dữ liệu string, phải parse để chuyển thành Object
+// let value = JSON.parse(localStorage.getItem("age"));
 
-console.log(value);
+// console.log(typeof value);
