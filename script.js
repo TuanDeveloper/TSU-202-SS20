5e86d31964760397bc4abfdd626d7299e9045819
Tuancoder2002 <it.coder.quoctuan@gmail.com>
2026-04-06 20:32:49 +0700

    storage

---
+++ script.js
@@ -0,0 +1,38 @@
+// let email = document.getElementById("email");
+// let password = document.getElementById("password");
+// let form = document.getElementById("form-login");
+
+// form.addEventListener("submit", function (event) {
+//     // Ngan chan hanh vi mac dinh cua form
+//     event.preventDefault();
+//     // let emailValue = email.value.trim();
+//     // let passwordValue = password.value.trim();
+
+//     let emailValue = form.email.value.trim();
+//     let passwordValue = form.password.value.trim();
+//     const newUser = {
+//         email: emailValue,
+//         password: passwordValue
+//     }
+//     console.log(newUser);
+// })
+
+let userName = "Quoc Tuan 123";
+let age = 30;
+
+let user = {
+    email: "it.coder.qtuan@gmail.com",
+    password: 123456,
+}
+
+localStorage.setItem("userName", userName);
+// localStorage.setItem("age", age);
+// Chuyển đổi object thành dữ liệu dạng JSON
+// Sau khi lưu vào Storage thì browser chuyển thành string và lưu
+localStorage.setItem("user", JSON.stringify(user));
+
+// localStorage.removeItem("userName");
+// Khi lấy về thì đang ở dữ liệu string, phải parse để chuyển thành Object
+let value = JSON.parse(localStorage.getItem("user"));
+
+console.log(value);
